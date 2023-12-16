87e6895afa0e7fa032ea66bc5e16f6e17dd971b6
Admin <sxcs7i2w24srqsxcrait2rfewrrk6t3ufjeb37v2odb5owrswatq>
2023-12-16 22:13:53 +0200

    added router module and fix issue user name

--- src/app/sheard/sheard.module.ts
+++ src/app/sheard/sheard.module.ts
@@ -1,33 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SheardComponent } from './sheard.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { HomeComponent } from './home/home.component';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:[
+  imports: [
     CommonModule,
     MatButtonModule, MatDividerModule
-    ,MatIconModule,HttpClientModule,FormsModule
-    ,MatSidenavModule,ReactiveFormsModule,MatPaginatorModule,NgxDropzoneModule
-    ,MatDialogModule],
-  declarations: [SheardComponent, SidebarComponent, NavbarComponent, HomeComponent,DeleteDialogComponent],
+    , MatIconModule, HttpClientModule, FormsModule
+    , MatSidenavModule, ReactiveFormsModule, MatPaginatorModule, NgxDropzoneModule
+    , MatDialogModule, RouterModule],
+  declarations: [SheardComponent, SidebarComponent, NavbarComponent, HomeComponent, DeleteDialogComponent],
 
-  exports:[ MatButtonModule,SidebarComponent ,NavbarComponent
-    ,HomeComponent,MatDividerModule, MatFormFieldModule,MatIconModule,MatSidenavModule,NgxDropzoneModule,HttpClientModule,DeleteDialogComponent,MatPaginatorModule,ReactiveFormsModule,MatDialogModule,FormsModule
+  exports: [MatButtonModule, SidebarComponent, NavbarComponent
+    , HomeComponent, MatDividerModule, MatFormFieldModule, MatIconModule,
+    MatSidenavModule, NgxDropzoneModule, HttpClientModule,
+    DeleteDialogComponent, MatPaginatorModule,
+    ReactiveFormsModule, MatDialogModule, FormsModule, RouterModule
   ]
 })
 export class SheardModule { }
